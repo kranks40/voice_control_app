8222f7ff6ef265cb6963bf31d457f727d094777e
kranks40 <eng_peters20@yahoo.com>
2021-01-04 22:02:15 -0400

    Haven't yet fixed the scrolling issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
 import wordsToNumbers from 'words-to-number'
 
 import alanBtn from '@alan-ai/alan-sdk-web';
--- src/components/styles.js
+++ src/components/styles.js
@@ -2,6 +2,8 @@ import { makeStyles } from '@material-ui/core/styles';
 
 
 export default makeStyles((theme) => ({
+
+    // Style for NewsCards component
     container: {
         padding: '0 5%',
         width: '100%',
@@ -26,16 +28,10 @@ export default makeStyles((theme) => ({
         textAlign: 'center',
     },
 
+ // styles for NewsCard component
+
     media: {
         height: 250,
-    },
-
-    border: {
-        border: 'solid'
-    },
-
-    fullHeightCard: {
-        height: '100%',
     },
 
     card: {
@@ -44,15 +40,6 @@ export default makeStyles((theme) => ({
         justifyContent: 'space-between',
         borderBottom: '10px solid white',
     },
-
-    activeCard: {
-        borderBottom: '10px solid #22289a',
-    },
-
-    grid: {
-        display: 'flex',
-    },
-
     details: {
         display: 'flex',
         justifyContent: 'space-between',
@@ -69,25 +56,79 @@ export default makeStyles((theme) => ({
         justifyContent: 'space-between',
     },
 
+    activeCard: {
+        borderBottom: '10px solid #22289a',
+    },
+
+    border: {
+        border: 'solid'
+    },
+
+    fullHeightCard: {
+        height: '100%',
+    },
+    
+    grid: {
+        display: 'flex',
+    },
+    //
+
     logoContainer: {
         padding: '0 5%',
         display: 'flex',
         justifyContent: 'space-around',
         alignItems: 'center',
         width: '100%',
-        // [theme.breakpoint.down('sm')]: {
-        //     flexDirection: 'column-reverse',
-        //     textAlign: 'center',
-        // },
     },
+
+
+
+
 
     alanLogo: {
         height: '27vmin',
         borderRadius: '15%',
         padding: '0 5%',
         margin: '3% 0',
-        // [theme.breakpoint.down('sm')]: {
-        //     height: '35vmin',
-        // },
     },
+
+// styles for Modal Component
+    paper: {
+        position: 'absolute',
+        backgroundColor: theme.palette.background.paper,
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(5),
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        outline: 'none',
+        overflow: 'scroll',
+        height: '60%',
+      },
+
+      infoContainer: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: '25px 0',
+      },
+
+      chipContainer: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        paddingLeft: '25px',
+        width: '50%',
+      },
+
+      trySaying: {
+        marginBottom: '25px',
+      },
+
+      chip: {
+        margin: '5px',
+      },
+      form: {
+        display: 'flex',
+        flexDirection: 'column',
+      },
 }));
